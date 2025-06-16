a394e8d56dca94ad20912843d1b865db436e2864
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-15 23:58:02 -0400

    se agregaron todos los botones + callbacks

--- lib/presentation/widgets/row_buttons.dart
+++ lib/presentation/widgets/row_buttons.dart
@@ -1,49 +1,146 @@
 import 'package:calculator_app/presentation/widgets/calculate_buttons.dart';
 import 'package:flutter/material.dart';
 
-class RowButtons extends StatefulWidget {
-  const RowButtons({super.key});
+class RowButtons extends StatelessWidget {
 
-  @override
-  State<RowButtons> createState() => _RowButtonsState();
-}
+  final String valueHistorial;
+  final String valueExpression;
 
-class _RowButtonsState extends State<RowButtons> {
+  // Metodos:
+  final Function onAllClear;
+  final Function onClear;
+  final Function onNumClick;
+  final Function onEvaluate;
 
-  // Metodo para limpiar los valores:
-  void allClearValues(String text) {
+  const RowButtons({
+    super.key, 
+    required this.valueHistorial,
+    required this.valueExpression, 
+    required this.onAllClear, 
+    required this.onClear, 
+    required this.onNumClick, 
+    required this.onEvaluate,
+  });
 
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return SizedBox(
+      child: Column(
         children: [
-          CalculateButtons(
-            valueCallBack: allClearValues,
-            valueText: "AC",
-            valueTextSize: 18,
-            valueColors: Colors.red.shade400,
-          ),
-          CalculateButtons(
-            valueCallBack: allClearValues,
-            valueText: "C",
-            valueTextSize: 18,
-          ),
-          CalculateButtons(
-            valueCallBack: allClearValues,
-            valueText: "%",
-            valueTextSize: 18,
-          ),
-          CalculateButtons(
-            valueCallBack: allClearValues,
-            valueText: "/",
-            valueTextSize: 18,
+
+          // Primera fila de botones:
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CalculateButtons(
+                valueCallBack: onAllClear,
+                valueText: "AC",
+                valueTextSize: 18,
+                valueColors: Colors.green.shade300,
+              ),
+              CalculateButtons(
+                valueCallBack: onClear,
+                valueText: "C",
+                valueTextSize: 18,
+                valueColors: Colors.red.shade400,
+              ),
+              CalculateButtons(
+                valueCallBack: onNumClick,
+                valueText: "%",
+                valueTextSize: 18,
+              ),
+              CalculateButtons(
+                valueCallBack: onNumClick,
+                valueText: "/",
+                valueTextSize: 18,
+              ),
+            ],
           ),
 
+          // Segunda fila de botones:
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CalculateButtons(
+                valueCallBack: onNumClick,
+                valueText: "7",
+                valueTextSize: 18,
+              ),
+              CalculateButtons(
+                valueCallBack: onNumClick,
+                valueText: "8",
+                valueTextSize: 18,
+              ),
+              CalculateButtons(
+                valueCallBack: onNumClick,
+                valueText: "9",
+                valueTextSize: 18,
+              ),
+              CalculateButtons(
+                valueCallBack: onNumClick,
+                valueText: "-",
+                valueTextSize: 18,
+              ),
+            ],
+          ),
 
+          // Segunda fila de botones:
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CalculateButtons(
+                valueCallBack: onNumClick,
+                valueText: "4",
+                valueTextSize: 18,
+              ),
+              CalculateButtons(
+                valueCallBack: onNumClick,
+                valueText: "5",
+                valueTextSize: 18,
+              ),
+              CalculateButtons(
+                valueCallBack: onNumClick,
+                valueText: "6",
+                valueTextSize: 18,
+              ),
+              CalculateButtons(
+                valueCallBack: onNumClick,
+                valueText: "+",
+                valueTextSize: 18,
+              ),
+            ],
+          ),
+
+          // Segunda fila de botones:
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CalculateButtons(
+                valueCallBack: onNumClick,
+                valueText: "1",
+                valueTextSize: 18,
+              ),
+              CalculateButtons(
+                valueCallBack: onNumClick,
+                valueText: "2",
+                valueTextSize: 18,
+              ),
+              CalculateButtons(
+                valueCallBack: onNumClick,
+                valueText: "3",
+                valueTextSize: 18,
+              ),
+              CalculateButtons(
+                valueCallBack: onEvaluate,
+                valueText: "=",
+                valueTextSize: 18,
+              ),
+            ],
+          ),
         ],
+        
+      ),
     );
   }
 }
