c7308636a62e84edc9da9ea7d86b52e67d8f4d7b
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-15 23:58:43 -0400

    se realizaron los metodos para pasarlos al row

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,7 +1,7 @@
-import 'package:calculator_app/presentation/widgets/result_screen.dart';
+import 'package:calculator_app/presentation/widgets/content_result_screen.dart';
 import 'package:calculator_app/presentation/widgets/row_buttons.dart';
 import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,6 +11,51 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  String _historial = "";
+  String _expression = "";
+
+  // Metodo para eliminar historial y expresion:
+  void onAllClear(String text){
+    setState(() {
+      _historial = "";
+      _expression = "";
+    });
+  }
+
+  // Metodo para eliminar la expresion:
+  void onClear(String text) {
+    setState(() {
+      _expression = "";
+    });
+  }
+
+  // Metodo para agregar valores:
+  void onNumClick(String text){
+    setState(() {
+      _expression += text;
+    });
+  }
+
+  // Metodo para realizar las operaciones:
+  void onEvaluate(String text) {
+
+    if (_expression.isEmpty) return;
+
+    // Si no contiene ningún operador, no hacer nada
+    if (!_expression.contains(RegExp(r'[+\-*/%]'))) return;
+
+    Parser valueParser = Parser();
+    Expression valueExpression = valueParser.parse(_expression);
+    ContextModel contModel = ContextModel();
+
+    setState(() {
+      _historial = _expression;
+      _expression = valueExpression.evaluate(EvaluationType.REAL, contModel).toString();
+    });
+  
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -29,24 +74,32 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Container(
             padding: const EdgeInsets.all(12),
           
-            child: const Column(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
           
                 // Contenedor de historial:
                 ContentResultScreen(
-                  valueText: 'Historial', 
+                  valueText: _historial, 
                   valueFontSize: 24, 
-                  valueColor: Color.fromARGB(255, 56, 57, 58),
+                  valueColor: const Color.fromARGB(255, 56, 57, 58),
                 ),
           
                 ContentResultScreen(
-                  valueText: "+", 
+                  valueText: _expression, 
                   valueFontSize: 48, 
                   valueColor: Colors.white
                 ),
           
-                RowButtons()
+                RowButtons(
+                  valueHistorial: _historial,
+                  valueExpression: _expression, 
+                  onAllClear: onAllClear,
+                  onClear: onClear,
+                  onNumClick: onNumClick,
+                  onEvaluate: onEvaluate,
+                ),
+
           
               ],
             ),
